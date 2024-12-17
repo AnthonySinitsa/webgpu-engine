1554f363bd06118589a43a9f43446c6fdadef148
AnthonySinitsa <itonyemail@gmail.com>
2024-12-17 03:51:56 +0000

    updated imgui

--- src/main.cpp
+++ src/main.cpp
@@ -60,47 +60,45 @@ static bool InitWGPU(GLFWwindow* window);
 static void CreateSwapChain(int width, int height);
 
 
-void renderCameraControls() {
-    ImGui::Begin("Camera Controls");
-    
+static void renderCameraControls() {
     bool cameraUpdated = false;
     
-    // Position controls
-    if (ImGui::DragFloat3("Position", &cameraState.position.x, 0.1f)) {
-        cameraUpdated = true;
-    }
-    
-    // Rotation controls (in degrees for easier understanding)
-    glm::vec3 rotationDegrees = glm::degrees(cameraState.rotation);
-    if (ImGui::DragFloat3("Rotation", &rotationDegrees.x, 1.0f)) {
-        cameraState.rotation = glm::radians(rotationDegrees);
-        cameraUpdated = true;
-    }
-    
-    // Projection controls
-    if (ImGui::SliderFloat("FOV", &cameraState.fov, 1.0f, 120.0f)) {
-        cameraUpdated = true;
-    }
-    
-    if (ImGui::DragFloat("Near Clip", &cameraState.nearClip, 0.1f, 0.1f, cameraState.farClip)) {
-        cameraUpdated = true;
-    }
-    
-    if (ImGui::DragFloat("Far Clip", &cameraState.farClip, 0.1f, cameraState.nearClip, 1000.0f)) {
-        cameraUpdated = true;
-    }
+    if (ImGui::CollapsingHeader("Camera Controls")) {
+        // Position controls
+        if (ImGui::DragFloat3("Position", &cameraState.position.x, 0.1f)) {
+            cameraUpdated = true;
+        }
+        
+        // Rotation controls (in degrees for easier understanding)
+        glm::vec3 rotationDegrees = glm::degrees(cameraState.rotation);
+        if (ImGui::DragFloat3("Rotation", &rotationDegrees.x, 1.0f)) {
+            cameraState.rotation = glm::radians(rotationDegrees);
+            cameraUpdated = true;
+        }
+        
+        // Projection controls
+        if (ImGui::SliderFloat("FOV", &cameraState.fov, 1.0f, 120.0f)) {
+            cameraUpdated = true;
+        }
+        
+        if (ImGui::DragFloat("Near Clip", &cameraState.nearClip, 0.1f, 0.1f, cameraState.farClip)) {
+            cameraUpdated = true;
+        }
+        
+        if (ImGui::DragFloat("Far Clip", &cameraState.farClip, 0.1f, cameraState.nearClip, 1000.0f)) {
+            cameraUpdated = true;
+        }
 
-    if (cameraUpdated) {
-        camera.setPerspectiveProjection(
-            glm::radians(cameraState.fov),
-            cameraState.aspectRatio,
-            cameraState.nearClip,
-            cameraState.farClip
-        );
-        camera.setViewYXZ(cameraState.position, cameraState.rotation);
+        if (cameraUpdated) {
+            camera.setPerspectiveProjection(
+                glm::radians(cameraState.fov),
+                cameraState.aspectRatio,
+                cameraState.nearClip,
+                cameraState.farClip
+            );
+            camera.setViewYXZ(cameraState.position, cameraState.rotation);
+        }
     }
-
-    ImGui::End();
 }
 
 
@@ -194,8 +192,7 @@ int main(int, char**)
 
     // Our state
     bool show_demo_window = false;
-    bool show_another_window = false;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    ImVec4 clear_color = ImVec4(0.f, 0.f, 0.f, 1.00f);
 
     // Initialize camera
     camera.setPerspectiveProjection(
@@ -252,42 +249,22 @@ int main(int, char**)
         ImGui_ImplGlfw_NewFrame();
         ImGui::NewFrame();
 
-        renderCameraControls();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
         if (show_demo_window)
             ImGui::ShowDemoWindow(&show_demo_window);
 
-        // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
         {
-            static float f = 0.0f;
-            static int counter = 0;
+            ImGui::Begin("Hierarchy");
 
-            ImGui::Begin("Hello, world!");                                // Create a window called "Hello, world!" and append into it.
+            ImGui::Checkbox("Demo Window", &show_demo_window);
 
-            ImGui::Text("This is some useful text.");                     // Display some text (you can use a format strings too)
-            ImGui::Checkbox("Demo Window", &show_demo_window);            // Edit bools storing our window open/close state
-            ImGui::Checkbox("Another Window", &show_another_window);
-
-            ImGui::SliderFloat("float", &f, 0.0f, 1.0f);                  // Edit 1 float using a slider from 0.0f to 1.0f
-            ImGui::ColorEdit3("clear color", (float*)&clear_color);       // Edit 3 floats representing a color
-
-            if (ImGui::Button("Button"))                                  // Buttons return true when clicked (most widgets return true when edited/activated)
-                counter++;
-            ImGui::SameLine();
-            ImGui::Text("counter = %d", counter);
+            ImGui::ColorEdit3("clear color", (float*)&clear_color);
+            
+            ImGui::Separator();
+            renderCameraControls();
+            ImGui::Separator();
 
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
-            ImGui::End();
-        }
-
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);         // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
             ImGui::End();
         }
 
