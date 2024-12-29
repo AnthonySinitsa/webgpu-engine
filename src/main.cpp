84f761671de5bb24297da5d5fcf2ce3e0eddbc16
AnthonySinitsa <itonyemail@gmail.com>
2024-12-29 04:52:18 +0000

    title

--- src/main.cpp
+++ src/main.cpp
@@ -188,7 +188,7 @@ int main(int, char**)
     // Make sure GLFW does not initialize any graphics context.
     // This needs to be done explicitly later.
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(wgpu_swap_chain_width, wgpu_swap_chain_height, "Dear ImGui GLFW+WebGPU example", nullptr, nullptr);
+    GLFWwindow* window = glfwCreateWindow(wgpu_swap_chain_width, wgpu_swap_chain_height, "WebGPU Engine", nullptr, nullptr);
     if (window == nullptr)
         return 1;
 
